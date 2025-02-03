f352f3cb169d5756bec50804a447007c38b30175
creamleeminsoo <dlalstn6302@naver.com>
2025-02-03 20:44:16 +0900

    update question3

--- question3.sql
+++ question3.sql
@@ -1,3 +1,6 @@
+
+-- 1, 결재 시스템 DB모델링
+
 CREATE TABLE user(
     user_id BIGINT AUTO_INCREMENT,
     auth_level CHAR(1) ,
@@ -6,6 +9,7 @@ CREATE TABLE user(
     PRIMARY KEY(user_id),
 
 );
+
 CREATE TABLE document(
     document_id BIGINT AUTO_INCREMENT,
     author_id BIGINT NOT NULL,
@@ -31,18 +35,34 @@ CREATE TABLE approval(
     CONSTRAINT chk_approval_status CHECK (approval_status IN ('0', '1', '2')),
     FOREIGN KEY (document_id) REFERENCES document(document_id) ON DELETE CASCADE,
     FOREIGN KEY (approver_id) REFERENCES user(user_id) ON DELETE CASCADE,
+
+    INDEX idx_approval_step_status_at(approval_step,approval_status,approval_at),
  
     PRIMARY KEY(approval_id)
 );
 
-SELECT 
-    doc.document_id AS document_id, 
-    doc.title AS document_title, 
-    appr.approval_step, 
+
+-- 2,특정 사용자가 처리해야 할 결재 건을 나열하는 query
+
+SELECT
+    appr.approval_at,
+    doc.created_at AS document_createdAt,
+    doc.document_id AS document_id,
+    doc.title AS document_title,
+    appr.approval_step,
     appr.approval_status 
-FROM document doc  
-JOIN approval appr ON doc.document_id = appr.document_id
-WHERE appr.approval_status = 0  
-AND appr.approval_step = (SELECT auth_level FROM user WHERE user_id = 0)
-ORDER BY doc.created_at ASC
-;
+FROM approval appr
+JOIN document doc ON appr.document_id = doc.document_id
+WHERE appr.approval_step = (SELECT auth_level FROM user WHERE user_id = 13)
+AND appr.approval_status = '0'
+ORDER BY appr.approval_at ASC;
+
+
+
+
+
+
+
+
+
+
